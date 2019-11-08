a11b8d68506c9881c87079196d2817ac4d7a1c71
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2019-11-08 14:22:51 +0100

    Add minimal websocket support and installation guide

---
+++ README.md
@@ -0,0 +1,9 @@
+Here is the back-end of the WTTJ test.
+
+## Installation
+
+```bash
+bundle
+
+ruby app.rb
+```
--- app.rb
+++ app.rb
@@ -1,5 +1,21 @@
 require 'sinatra'
+require 'sinatra-websocket'
+
+set :server, 'thin'
+set :sockets, []
 
 get '/' do
-  'Hello world!'
+  request.websocket do |ws|
+    ws.onopen do
+      ws.send("coucou")
+      settings.sockets << ws
+    end
+
+    ws.onmessage do |msg|
+      EM.next_tick { settings.sockets.each{|s| s.send(msg) } }
+    end
+    
+    ws.onclose { settings.sockets.delete(ws) }
+  end
 end
+
